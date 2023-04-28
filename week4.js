843f95cf4a28aa53a1f4d0c506f763a0cbae5acb
Natalie Childs <nata.faye@gmail.com>
2023-04-27 18:40:43 -0700

    emails practice and arrow functions

--- week4.js
+++ week4.js
@@ -1,75 +1,4 @@
 
-
-// for(let i = 0; i < fdsfds; i++) {
-//     const message = "This is looping " + i
-//     alert(message)
-// }
-
-
-
-// function add(a, b) {
-//     return a + b
-// }
-
-// const add = (a, b) => {
-//     return a + b
-// }
-
-//const add = (a, b) => a + b
-
-//const double = (a) => a + a
-const double = a => a + a
-
-
-
-
-// function isNotEmpty(paper) {
-//     return paper !== '';
-// }
-
-// const isNotEmpty = (paper) => {
-//     return paper !== '';
-// }
-
-// const isNotEmpty = (paper) => paper !== ''
-
-// const isNotEmpty = paper => paper !== ''
-
-// const isNotEmpty = p => p !== ''
-
-// p => p !== ''
-
-
-
-
-// const doesNameStartWithM = name => name[0] === "M"
-
-// doesNameStartWithM("Maria")
-
-
-
-// function map(array, callback) {
-//     const mappedArray = []
-//     for(let i = 0; i < array.length; i++) {
-//         mappedArray.push(callback(array[i]))
-//     }
-//     return mappedArray
-// }
-
-
-function filter(array, callback) { //callback = email => !email.read
-    const filteredArray = []
-    for(let i = 0; i < array.length; i++) {
-        if(callback(array[i])) {
-            filteredArray.push(array[i])
-        }
-    }
-    return filteredArray
-}
-
-
-
-
 let emailList = [
     {
         id: 234,
@@ -94,47 +23,83 @@ let emailList = [
     }
 ]
 
-//emailList[0].read
+// [
+//     "Heyyyy",
+//     "What's up?",
+//     "Good day"
+// ]
 
-//displayStrings = ["Heyyyy", "What's up?", "Good day"]
+// Map - data to how that data should be displayed
 
+// const emailMessagesList = emailList.map(email => email.message)
+// const stringOfMessages = emailMessagesList.join("\n") // \n means enter
+// alert(stringOfMessages)
 
-// MAP - map data (objects) to how we're going to display it (string)
-//const displayStrings = emailList.map( email => email.author + ": " + email.message )
-alert(emailList.map( email => `${email.author}: ${email.message}` ).join("\n"))
-
-emailList.push({ 
-    id: 2000,
-    author: "Dylan",
-    to: "Calvin",
-    message: "Good day",
-    read: false, 
-})
-
-alert(emailList.map( email => `${email.author}: ${email.message}` ).join("\n"))
-
-// FILTER - filter for unread emails
-
-// Very expanded version:
-function isEmailUnread(email) {
-    return !email.read
-}
-function turnEmailObjectToString(email) {
-    return `${email.author}: ${email.message}`
-}
-const filteredEmailsArrayOfOBjects = emailList.filter( isEmailUnread )
-console.log("filteredEmailsArrayOfOBjects", filteredEmailsArrayOfOBjects)
-const filteredEmailsArrayOfStrings = filteredEmailsArrayOfOBjects.map( turnEmailObjectToString )
-console.log("filteredEmailsArrayOfStrings", filteredEmailsArrayOfStrings)
-const stringOfAllFilteredEmails = filteredEmailsArrayOfStrings.join("\n")
-console.log("stringOfAllFilteredEmails", stringOfAllFilteredEmails)
-alert("UNREAD\n" + stringOfAllFilteredEmails)
-
-// Streamlined version:
-alert("UNREAD\n" + emailList.filter(email => !email.read).map(email => `${email.author}: ${email.message}`).join("\n"))
+//alert(emailList.map(email => email.author + ": " + email.message).join("\n"))
 
 
-// FIND- find one particular item by its
-const emailToShow = emailList.find( e => e.id === 1654 )
-const emailAuthorToShow = emailList.find( e => e.id === 1654 ).author
-alert("EMAIL TO SHOW\n" + `${emailToShow.author}: ${emailToShow.message}`)
+// Filter - filter (read emails, matches a search, folders, filters)
+
+//const unreadEmails = emailList.filter(   email =>  email.read === false  )
+//alert("Unread Emails:\n\n" + unreadEmails.map(email => email.author + ": " + email.message).join("\n"))
+
+
+alert("Unread Emails:\n\n" + emailList // array of all emails
+    .filter(   email =>  email.read === false  ) // array of 2 emails
+    .map(email => email.author + ": " + email.message) // array of 2 strings
+    .join("\n") // one string
+)
+// Find - find (find by id)
+
+const email = emailList.find(email => email.id === 474)
+alert("Here's the email with that id: " + email.message)
+
+
+
+
+
+// Great if this isn't a callback function
+// function sayHi(name) {
+// 	return "Hi " + name
+// }
+
+// // not the greatest practice, because there's only one line
+// const sayHi = (name) => {
+//     return "Hi " + name
+// }
+
+// // Have to have the curly brackets
+// const sayHi = (name) => {
+//     const longName = name + " is there name "
+//     return "Hi " + longName
+// }
+
+// // either of these is great
+// const sayHi = (name) => "Hi " + name
+
+// const sayHi = name => "Hi " + name
+
+
+
+// // const isNotEmpty = (paper) => {
+// //     return paper !== '';
+// //  }
+
+// //  const isNotEmpty = (paper) => paper !== ''
+
+// //  const isNotEmpty = p => p !== ''
+ 
+
+//  const numbers = [0, 1, 2, 34]
+
+//  const highNumbers = numbers.filter(num => num > 2)
+//  const lowNumbers = numbers.filter(num => num < 2)
+
+
+//  let total = 0
+
+//  for(let i = 0; i < 5; i++) {
+//     total = total + i
+//  }
+
+//  alert(total)
