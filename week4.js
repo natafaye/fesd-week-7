154e016393739a7ffa4e52249b4b8113b43bbb16
Natalie Childs <nata.faye@gmail.com>
2023-06-27 20:28:08 -0700

    Missed a spot

--- week4.js
+++ week4.js
@@ -1,63 +1,63 @@
 
 
-// const sayHi = () => "fdsfds"
+const sayHi = () => "fdsfds"
 
-// // Use an arrow functione very time:
-// // 1) If it's one line
-// // 2) callback function
+// Use an arrow functione very time:
+// 1) If it's one line
+// 2) callback function
 
-// // Exactly one line
-// // return and curly brackets go together
-// const addFive = (number) => number + 5
+// Exactly one line
+// return and curly brackets go together
+const addFive = (number) => number + 5
 
-// // Don't do this (probably)
-// // Makes you look like you're not comfortable with arrow functions
-// const addSeven = (number) => {
-//     return number + 7
-// }
+// Don't do this (probably)
+// Makes you look like you're not comfortable with arrow functions
+const addSeven = (number) => {
+    return number + 7
+}
 
-// console.log(addFive(3))
+console.log(addFive(3))
 
-// // Exactly one parameter
-// // Don't have to remove the parenthesis I usually keep them
-// const addSix = number => number + 6
-
-
+// Exactly one parameter
+// Don't have to remove the parenthesis I usually keep them
+const addSix = number => number + 6
 
 
-// //  const full = papers.filter( p => p !== '' )
 
 
-// // higher order function = a function that has a callback parameter (accepts a callback function as a paramter)
-//  const doMathWith3And5AndGiveAnswer = (mathCallback) => { // mathCallback = (a, b) => a - b - 5
-//     const result = mathCallback(3, 5)
-//     alert(result)
-//  }
-
-//  doMathWith3And5AndGiveAnswer(  (firstNumber, secondNumber) => firstNumber - secondNumber  )
-
-//  const add = (a, b) => a + b
-//  doMathWith3And5AndGiveAnswer(add)
-
-//  doMathWith3And5AndGiveAnswer("hello") // ERROR
-
-//  const subtract = (a, b) => a - b - 5
-//  doMathWith3And5AndGiveAnswer(subtract)
-
-//  // higher order function
-//  const fancyTotalOfArray = (array, callback) => {
-//     const total = 0
-//     for(const item in array) {
-//         total += callback(item)
-//     }
-//     return total
-//  }
-
-//  // call higher order function and give it a callback function
-//  fancyTotalOfArray([1, 2, 3], (number) => number * number)
+//  const full = papers.filter( p => p !== '' )
 
 
- let emailList = [
+// higher order function = a function that has a callback parameter (accepts a callback function as a paramter)
+const doMathWith3And5AndGiveAnswer = (mathCallback) => { // mathCallback = (a, b) => a - b - 5
+    const result = mathCallback(3, 5)
+    alert(result)
+}
+
+doMathWith3And5AndGiveAnswer((firstNumber, secondNumber) => firstNumber - secondNumber)
+
+const add = (a, b) => a + b
+doMathWith3And5AndGiveAnswer(add)
+
+doMathWith3And5AndGiveAnswer("hello") // ERROR
+
+const subtract = (a, b) => a - b - 5
+doMathWith3And5AndGiveAnswer(subtract)
+
+// higher order function
+const fancyTotalOfArray = (array, callback) => {
+    const total = 0
+    for (const item in array) {
+        total += callback(item)
+    }
+    return total
+}
+
+// call higher order function and give it a callback function
+fancyTotalOfArray([1, 2, 3], (number) => number * number)
+
+
+let emailList = [
     {
         id: 234,
         author: "Natalie",
@@ -91,18 +91,18 @@
 
 // Map
 // arrays of data to how that data will be displayed
-alert( emailList.map(email => email.author + ":\n" + email.message ).join("\n\n") ) // \n = new line (enter)
+alert(emailList.map(email => email.author + ":\n" + email.message).join("\n\n")) // \n = new line (enter)
 
 
 // Filter
 // filter for emails that are unread
-const unreadEmails = emailList.filter(  (email) => !email.read  )
+const unreadEmails = emailList.filter((email) => !email.read)
 
-alert( "UNREAD EMAILS\n\n" + unreadEmails.map(email => email.author + ":\n" + email.message ).join("\n\n") )
+alert("UNREAD EMAILS\n\n" + unreadEmails.map(email => email.author + ":\n" + email.message).join("\n\n"))
 
 // Find
 // find by id
-const specialEmail = emailList.find( email => email.id === 474 )
+const specialEmail = emailList.find(email => email.id === 474)
 console.log(specialEmail)
 alert("SPECIAL EMAIL\n\n" + specialEmail.author + ":\n" + specialEmail.message)
 
