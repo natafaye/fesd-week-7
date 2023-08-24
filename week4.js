fe4c123ff9916e775ee18c9950b43f1278dbc710
Natalie Childs <nata.faye@gmail.com>
2023-08-23 19:47:53 -0700

    callback and email examples

--- week4.js
+++ week4.js
@@ -1,60 +1,68 @@
 
 
-const sayHi = () => "fdsfds"
+// Use const every time you can and let only if it will be changed
+// const username = prompt("What is your username?")
 
-// Use an arrow functione very time:
-// 1) If it's one line
-// 2) callback function
-
-// Exactly one line
-// return and curly brackets go together
-const addFive = (number) => number + 5
-
-// Don't do this (probably)
-// Makes you look like you're not comfortable with arrow functions
-const addSeven = (number) => {
-    return number + 7
-}
-
-console.log(addFive(3))
-
-// Exactly one parameter
-// Don't have to remove the parenthesis I usually keep them
-const addSix = number => number + 6
+// // INTENDED: if username is equal to natalie alert you're the best
+// // ACTUALLY DOING: set username to "natalie" then alert "You're the best"
+// if(username === "natalie") {
+//     alert("You're the best")
+// }
 
 
+// function checkUsername(user) {
+//     return user.username === "jose"
+// }
+
+// // Convert it to an arrow function
+// const checkUsername = (user) => {
+//     return user.username === "jose"
+// }
+
+// // Advice: always do this one
+// // If you have exactly one line in the body of the function, you can get rid of the curly brackets and the return
+// const checkUsername = (user) => user.username === "jose"
+
+// // Advice: only do this one if it makes it more readable to you
+// // If you have exactly one parameter you can remove the parenthesis
+// const checkUsername = user => user.username === "jose"
 
 
-//  const full = papers.filter( p => p !== '' )
+// When to use arrow functions
+//1) When you feel like it
+//2) When it's a callback function
+//3) When it only has one line in it -> make it a one line arrow function
 
-
-// higher order function = a function that has a callback parameter (accepts a callback function as a paramter)
-const doMathWith3And5AndGiveAnswer = (mathCallback) => { // mathCallback = (a, b) => a - b - 5
-    const result = mathCallback(3, 5)
-    alert(result)
-}
-
-doMathWith3And5AndGiveAnswer((firstNumber, secondNumber) => firstNumber - secondNumber)
-
-const add = (a, b) => a + b
-doMathWith3And5AndGiveAnswer(add)
-
-doMathWith3And5AndGiveAnswer("hello") // ERROR
-
-const subtract = (a, b) => a - b - 5
-doMathWith3And5AndGiveAnswer(subtract)
 
 // higher order function
-const fancyTotalOfArray = (array, callback) => {
-    const total = 0
-    for (const item in array) {
-        total += callback(item)
-    }
-    return total
-}
+// const doSomethingToNumbers = (number1, number2, callback) => {
+//     const result = callback(number1, number2)
+//     console.log(result)
+// }
 
-// call higher order function and give it a callback function
-fancyTotalOfArray([1, 2, 3], (number) => number * number)
+// const add = (a, b) => a + b
+
+// doSomethingToNumbers(5, 3, add) // add is a callback function right here
+
+
+// function startSomething() {
+//     console.log("We're starting")
+//     doSomethingToNumbers(5, 3, (a, b) => {
+//         let result = a
+//         result += 5
+//         result += 5
+//         for(let i = 0; i < a; i++) {
+//             result += b
+//         }
+//         return result
+//     })
+
+
+//     return "hello"
+// }
+
+
+
 
 
 let emailList = [
@@ -81,28 +89,44 @@ let emailList = [
     }
 ]
 
-//  [
-//     "Calvin:\nHeyyyy",
-//     "Natalie:\nWhat's up?",
-//     "Dylan:\nGood day"
+// [
+//     "Natalie: Heyyyy",
+//     "Natalie",
+//     "Dylan"
 // ]
 
-// "Calvin:\nHeyyyy\n\nNatalie:\nWhat's up?\n\nDylan:\nGood day"
+// Map - most common use is mapping an array of objects to how those objects should be displayed
+alert(emailList.map( email => email.author + ": " + email.message ).join("\n")) // \n = Enter
 
-// Map
-// arrays of data to how that data will be displayed
-alert(emailList.map(email => email.author + ":\n" + email.message).join("\n\n")) // \n = new line (enter)
+// map knows it's working with emailList
+
+// map grabs the first item in emaillist
+// callback({ id: 234, author: "Natalie" and more }) // callback is    email => email.author + ": " + email.message
+// the string that's spit out "Natalie: Heyyyy" is put in the new array
+
+// map grabs the next item in emaillist
+// callback({ id: 1654, author: "Natalie" and more }) // callback is    email => email.author + ": " + email.message
+// the string that's spit out "Natalie: What's up" is put in the new array
+
+// map grabs the next item in emaillist
+// callback({ id: 474, author: "Dylan" and more }) // callback is    email => email.author + ": " + email.message
+// the string that's spit out "Dylan: Good day" is put in the new array
+
+// returns that array with all the strings ["Natalie: Heyyyy", "Natalie: What's up", "Dylan: Good day"]
 
 
-// Filter
-// filter for emails that are unread
-const unreadEmails = emailList.filter((email) => !email.read)
+// Filter - most common use is to filter things (used for search)
 
-alert("UNREAD EMAILS\n\n" + unreadEmails.map(email => email.author + ":\n" + email.message).join("\n\n"))
+const unreadEmails = emailList.filter( email => !email.read )
 
-// Find
-// find by id
+// copied line to display it
+alert("Unread Emails\n\n" + unreadEmails.map( email => email.author + ": " + email.message ).join("\n"))
+
+
+// Find - most common use is to find by the id
+
+// Get the email with the id of 474
+
 const specialEmail = emailList.find(email => email.id === 474)
-console.log(specialEmail)
-alert("SPECIAL EMAIL\n\n" + specialEmail.author + ":\n" + specialEmail.message)
 
+alert("Special Email\n\n " + specialEmail.author + ": " + specialEmail.message)
