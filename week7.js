88f9f701e3ef9702f3bccfd9d3afd3d99fe3059a
Natalie Childs <nata.faye@gmail.com>
2025-10-14 19:07:56 -0700

    toasters and books

--- week7.js
+++ week7.js
@@ -1,12 +1,12 @@
-// console: '🎫'
+// CONSOLE: '🎫'
 
 function getTicket(money) { // let money = 10
   if (money >= 10) {
-    return '🎫'
+    return '🎫' //console.log('🎫')
   }
 }
 
-function enterTheater(movieTicket) { // let movieTicket = '🎫'
+function enterTheater(movieTicket) { // let movieTicket = undefined
   if (movieTicket === '🎫') {
     alert('Enjoy your movie!')
   } else {
@@ -14,37 +14,104 @@ function enterTheater(movieTicket) { // let movieTicket = '🎫'
   }
 }
 
-let ticket = '🎫'
-enterTheater('🎫')
-
-// strings, numbers, booleans are small, and just go straight in the variable
-// If your friend asks you for a picture of yourself you just text it to them, if they ask for a shirt you hand it to them
-
-// objects and arrays are too big to go straight in, so instead the variable just points to them
-// If your friend asks you for your favorite restaurant, you point to it (with an address maybe)
+// Function calls simplify to whatever that function returns
+// If the function has no return statement it simplifies to undefined
+const ticket = getTicket(10) // WAIT
+enterTheater(ticket)
 
 
+// DATA TYPES
 
-function filter(array, shouldKeep) {
-  // let array = ["apple", "berry", "banana"]; 
-  // let shouldKeep = (food) => food.includes("a")       let food = "banana"
-  const filteredArray = [] // [ "apple", "banana" ]
-  for (let i = 0; i < array.length; i++) {
-    const item = array[i] // "banana"
-    if (shouldKeep(item)) {
-      filteredArray.push(item)
-    }
-  }
-  return filteredArray
+// BASIC
+// bool, string, number
+// Actually in the variable
+
+// FANCY
+// object, array
+// too big to actually be "in" the variable
+// pointed to
+
+
+
+// const and let make a new variable
+// Scope: wherever that const or let is, the variable is trapped inside there
+// You can have two variables with the same name if they're in different scopes
+// When you use a variable name it looks for the "closest" line with a let or const and that variable name
+// STRONG GUIDELINE: Don't use the same name for a variable in overlapping scope
+// We do reuse variable names constantly and we should to make it easy to understand what's in it
+
+let myString = "Here is an equation + (5 + 3) + isn't it great"
+let mySecondString = "Here is an equation " + (5 + 3) + " isn't it great"
+let myThirdString = `Here is an equation ${5 + 3} isn't it great`
+
+console.log(myString)
+console.log(mySecondString)
+console.log(myThirdString)
+
+
+// The most confusing thing is that input is a variable (specifically a parameter)
+// Somewhere this happens: let input = WHATEVER THEY PASS IN
+// const myFunction = input => "output"
+// // c u l8r
+
+// function myFunction(input) {
+//   return "output"
+// }
+// see you later
+
+
+
+
+const teach = () => alert("smart things")
+
+// ALERT: "smart things"
+
+function getTeachFunction() {
+  // On a line with a return the first thing it will do is simplify the part after the return to a value
+  // To simplify a function call it calls the function and simplifies to whatever that function returns
+  return teach
 }
 
+// teach is a toaster
+// teach() is toast
 
-const fruits = ["apple", "😆", "banana"]
-const fullStrings = filter(fruits, food => food.includes("a"))
+//  log out what getTeachFunction returns
+console.log(getTeachFunction()) // () => alert("smart things")
+
+
+//specialFunction(teach, isDeveloper)
 
 
 
 
+
+// function filterOutEmpty(array) {
+//   const filteredArray = []
+//   for (let i = 0; i < array.length; i++) {
+//     const item = array[i]
+//     if (item < 100) {
+//       filteredArray.push(item)
+//     }
+//   }
+//   return filteredArray // ["apple", "banana"]
+// }
+
+// const fruits = ["apple", "", "banana", "", ""]
+// const fullStrings = filterOutEmpty(fruits)
+
+
+// const prices = [10, 150, 40, 37]
+// const cheaperPrices = filterOutEmpty(prices)
+
+
+// In professional I almost never see a for loop only .map, .filter, .find, .foreach
+
+
+// THE BIG CONCEPTS
+// Array methods (map, filter, find) with arrays of objects
+//     Callbacks
+//     Functions calling other functions
+//     Navigating inside of arrays of objects
 
 
 
@@ -71,17 +138,20 @@ const books = [
   },
 ]
 
-// ["Jane Eyre", "Ancillary Justice", "Atomic Habits", "Dune"]
-
-books[1].title // "Ancillary Justice"
-
+// Map - map each book in this style: "Jane Eyre (Classic)"
 // Filter - filter for all Science Fiction books (Ancillary Justice, Dune)
+// Find - find the book with the id of 2 (Atomic Habits)
 
+
+// MAP - most common use of map is to map objects to how we want them displayed
+// Callback that takes in each book as input and returns the string in this format "Title (Genre)"
+alert("Here are the boooks " + books.map(  book => book.title + " (" + book.genre + ")"  )  )
+
+// FILTER - most common use of filter is to filter or search
 const scienceFictionBooks = books.filter( book => book.genre === "Science Fiction" )
 console.log(scienceFictionBooks)
 
 
-// Map - map each book in this style: "Jane Eyre (Classic)"
-// We frequently use map to map an array of objects to a nice way to display them
-
-alert(   books.map(  book => `${book.title} (${book.genre})`  )   )
+// FIND - most common use of find is to find one with a particular id
+const specificBook = books.find( book => book.id === 3 )
+console.log(specificBook)
